32c73fb2f5e99e617751ea9ce4d3b66c7a9bbe8f
Muhammed <muhammed.oasln@gmail.com>
2023-06-02 16:21:01 +0300

    purple-white

---
+++ index.js
@@ -0,0 +1,18 @@
+document.querySelector('.btn').onclick = function(){
+
+
+
+const col1 = document.getElementById('divOne').style.backgroundColor;
+
+if (col1 === 'indigo') {
+    document.getElementById('divOne').style.backgroundColor = 'white'
+    document.querySelector('.btn').innerText = 'White'
+}
+else{document.getElementById('divOne').style.backgroundColor = "indigo";
+document.querySelector('.btn').innerText = 'Purple'
+
+}
+
+}
+
+
