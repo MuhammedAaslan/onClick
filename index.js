c8ae5ee7b60dc2e7fad19c9270f441b88f744502
Muhammed <muhammed.oasln@gmail.com>
2023-06-02 18:00:26 +0300

    newIUpdate

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-document.querySelector('.btn').onclick = function(){
+document.querySelector('.btn').onclick = function () {
 
 
 
-const col1 = document.getElementById('divOne').style.backgroundColor;
+    const col1 = document.getElementById('divOne').style.backgroundColor;
 
-if (col1 === 'indigo') {
-    document.getElementById('divOne').style.backgroundColor = 'white'
-    document.querySelector('.btn').innerText = 'White'
+    if (col1 === 'indigo') {
+        document.getElementById('divOne').style.backgroundColor = 'white'
+        document.querySelector('.btn').innerText = 'Click'
+    }
+    else {
+        document.getElementById('divOne').style.backgroundColor = "indigo";
+        document.querySelector('.btn').innerText = 'Purple'
+
+    }
 }
-else{document.getElementById('divOne').style.backgroundColor = "indigo";
-document.querySelector('.btn').innerText = 'Purple'
 
-}
 
-}
 
 
